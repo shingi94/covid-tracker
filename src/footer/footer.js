2f180c101d832f8c61caef779abfabac42becc2f
Shingidzano Lesetedi <vergil.sp94@live.com>
2021-01-29 08:40:42 +0200

    Final commit of completed app

---
+++ src/footer/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './footer.module.css';
+
+
+const Footer = () => (
+  
+  <div className={styles.footer}>
+    <p>This project was developed by Shingidzano Lesetedi</p>
+
+  </div>
+);
+
+
+export default Footer;
